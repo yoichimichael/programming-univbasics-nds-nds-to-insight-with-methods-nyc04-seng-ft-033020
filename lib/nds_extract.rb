4a47a12f9aacb9542df9293efb5f8823b5de7362
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-25 04:44:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+pp directors_database
+
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
