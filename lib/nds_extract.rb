43066450a5261bbb91ced2a8fab9f7c697c243b3
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-25 04:48:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,11 @@ pp directors_database
 # using director_data as input
 def gross_for_director(director_data)
   gross_of_movies = 0
-  
+  movie_count = 0
+  while movie_count < director_data[:movies].length do
+    gross_of_movies += director_data[:movies][movie_count][:worldwide_gross]
+    movie_count += 1 
+  end
   gross_of_movies
 end
 
