f7ec52def72c34ec9ef45a51facc2114fad24440
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-25 04:55:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,7 +23,8 @@ def directors_totals(nds)
   result = {}
   director_count = 0
   while director_count < nds.length do
-    result[nds[director_count][:]
+    result[nds[director_count][:name]] = gross_for_director(nds[director_count])
+    director_count += 1 
   end
   result
 end
